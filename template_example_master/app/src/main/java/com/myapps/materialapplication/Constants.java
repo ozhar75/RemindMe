2d7628f4c013908544d20d5f4b4bbde9fdae1e6d
Akshay Dhonde <ozhar75@gmail.com>
2017-07-04 12:27:21 +0530

    Update Constants.java

--- template_example_master/app/src/main/java/com/myapps/materialapplication/Constants.java
+++ template_example_master/app/src/main/java/com/myapps/materialapplication/Constants.java
@@ -4,6 +4,6 @@ package com.myapps.materialapplication;
  * Created by ozhar on 21-04-2016.
  */
 public class Constants {
-    public static final String FIREBASE_HOME_URL = "https://medremind.firebaseio.com/";
+    public static final String FIREBASE_HOME_URL = "url of your firebase database";
 
 }
